20fd4e7fb13a5d390613ecd38ac94db7ee0f8bf2
sravanpluselve <sravankumar.p@pluselve.com>
2018-10-04 17:18:21 +0530

    [new] initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Rob', 'rob@test.com', 65465456456, '', 'morning', true);
+}
